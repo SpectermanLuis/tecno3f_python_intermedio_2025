1741b45afcf0645714912c60b10758bfebef843a
SpectermanLuis <110212328+SpectermanLuis@users.noreply.github.com>
2025-10-01 20:33:30 -0300

    Add files via upload
    Archivos base sobre los que se resuelve la tarea solicitada

---
+++ CLASE05/ORIGINALES/test.py
@@ -0,0 +1,7 @@
+from cb import CuentaBancaria
+
+#Esto solo es un ejemplo para que vean como ingresar la fecha
+
+cuenta1 = CuentaBancaria("Gabriel",333333,'1990/02/03',15000)
+
+print(cuenta1.obtener_edad())
